96421845018ba9c76bd4ff0bc1c6c85df2760513
Sandhya <sandhya@Sandhyas-Air.ht.home>
2024-12-02 08:04:55 -0600

    day 1 part 2

--- day-1/solution.py
+++ day-1/solution.py
@@ -1,5 +1,4 @@
-# solution
-# read text file
+## Part 1
 # read each column in text file into two sorted lists
 # find pairwise different between the lists and add the difference
 # that's the total distance between the two lists
@@ -23,4 +22,33 @@ def get_total_distance(filename):
     return total_dist
 
 total = get_total_distance("input.txt")
-print(total)
+print("total_distance", total)
+
+## Part 2
+# read column 1 as list 1
+# read column2 as frequency dict where you know how many times each item appears in list 2
+# iterate over list 1 and multiply it by the item's frequency and add them all
+# that's the total similarity of two lists
+
+def get_similarity(filename):
+    list1 = []
+    item2freq = {}
+
+    with open(filename, 'r') as file:
+        for line in file:
+            values = line.strip().split()
+            list1.append(int(values[0]))
+            item2 = int(values[1])
+            if item2 in item2freq:
+                item2freq[item2] += 1
+            else:
+                item2freq[item2] = 1
+
+    similarity_score = 0
+    for item in list1:
+        similarity_score += (item * item2freq.get(item, 0))
+    
+    return similarity_score
+
+sim_score = get_similarity("input.txt")
+print("similarity_score", sim_score)
