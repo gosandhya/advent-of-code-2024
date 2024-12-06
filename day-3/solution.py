fa2a8ad017e70b533138e685dacf823f65244af5
Sandhya <sandhya@Sandhyas-Air.ht.home>
2024-12-06 09:19:59 -0600

    day 3 part 2

--- day-3/solution.py
+++ day-3/solution.py
@@ -1,9 +1,12 @@
 import re
-def find_pattern(input):
+
+def read_file(input):
     with open(input, "r") as file:
         content = file.read()
-    pattern = r"mul\(\d+,\d+\)"
+    return content
 
+def find_pattern(content):
+    pattern = r"mul\(\d+,\d+\)"
     matches = re.findall(pattern, content)
 
     return matches
@@ -16,6 +19,31 @@ def calculations(mul_list):
         total_sum+=mul
     return total_sum
 
-matches = find_pattern("input.txt")
+content = read_file("input.txt")
+matches = find_pattern(content)
 total_sum = calculations(matches)
 print(total_sum)
+
+test_str = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+#pattern = r"(don't\(\))|(do\(\))"
+
+def find_conditional_pattern(content):
+    splitted = re.split(r"don't\(\)", content)
+    filtered_content = ""
+    for i, v in enumerate(splitted):
+        if i == 0:
+            filtered_content+=v
+            continue
+        split_2 = re.split(r"do\(\)", v)
+        if len(split_2) == 1:
+            continue
+        else:
+            filtered_content+="".join(split_2[1:])
+    return filtered_content
+
+
+filtered_content = find_conditional_pattern(content)
+matches = find_pattern(filtered_content)
+filtered_sum = calculations(matches)
+print(filtered_sum)
+
